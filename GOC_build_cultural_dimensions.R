5ee92904f3397ef587b7fc35411d9267b425a350
moveslike15 <67294829+moveslike15@users.noreply.github.com>
2020-08-01 01:21:27 +0900

    Update GOC_build_cultural_dimensions.R

--- GOC_build_cultural_dimensions.R
+++ GOC_build_cultural_dimensions.R
@@ -1,70 +1,95 @@
-##year_1 and year_2 comprise the time period of ngrams to read##
-year_1=2000
-year_2=2012
+library("ggplot2")
+memory.limit(1500000)
 
-import numpy
-import scipy
-import cython
-import gensim
-from gensim import models, similarities
-import logging
-import os
-import re
-import sys  
-import itertools
-import math
 
-#reload(sys)  
-#sys.setdefaultencoding('utf8')
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+survey<-read.csv(file="/DIRECTORY/survey_dataset_means_weighted.csv",header=TRUE,row.names=1)
+df<-read.csv(file="/DIRECTORY/GoogleNews_Embedding.csv", header=TRUE,row.names=1, sep=",")
+#df<-read.csv(file="/DIRECTORY/US_Ngrams_2000_12.csv", header=TRUE,row.names=1, sep=",")
 
-class MySentences(object):
-    def __init__(self, dirname, start_year, end_year, limit=None):
-        self.dirname = dirname
-        self.start_year = start_year
-        self.end_year = end_year
-        self.limit = limit
- 
-    def __iter__(self):
-         # iterate through each the compressed file directory
-        for fname in os.listdir(self.dirname):
-            # for each compressed file open it
-            with gensim.utils.smart_open(os.path.join(self.dirname, fname)) as fin:
-                for line in itertools.islice(fin, self.limit):
-                    line = gensim.utils.to_unicode(line).split("\t")
-                    if len(line)<3:
-                        continue
-                    ngram = line[0]
-                    try:
-                        year = int(line[1])
-                    except ValueError:
-                        continue
-                    match_count = int(line[2])
-                    if year < self.start_year or year > self.end_year:
-                        continue
-                    # lower case the ngram, remove pos
-                    processed_ngram = [word.split("_")[0] for word in ngram.lower().split()]
-                    for x in range(match_count):
-                        yield processed_ngram
+#####DEFINE FUNCTIONS##########
+#Calculate norm of vector#
+norm_vec <- function(x) sqrt(sum(x^2))
 
-assert gensim.models.word2vec.FAST_VERSION > -1
-##Reads ngrams one-by-one into word2vec##
-sentences = MySentences("/dir_name_here/", year_1, year_2) # a memory-friendly iterator
+#Dot product#
+dot <- function(x,y) (sum(x*y))
 
-###Set parameters. Details here: https://radimrehurek.com/gensim/models/word2vec.html###
-model = gensim.models.word2vec.Word2Vec(sentences,sg=1, size=300, window=5, min_count=10, workers=10, hs=0, negative=8)
-model.save('w2vmodel_ng5_'+str(year_1)+'_'+str(year_2)+'_full')
-syn0_object=model.wv.syn0
+#Cosine Similarity#
+cos <- function(x,y) dot(x,y)/norm_vec(x)/norm_vec(y)
 
-##output vector space##
-numpy.savetxt('syn0_ngf_'+str(year_1)+'_'+str(year_2)+'_full.txt',syn0_object,delimiter=" ")
+#Normalize vector#
+nrm <- function(x) x/norm_vec(x)
 
-#output vocab list#
-vocab_list = model.wv.index2word
-for i in range(0,len(vocab_list)):
-	if vocab_list[i] == '':
-		vocab_list[i] = "thisisanemptytoken"+str(i)
+#Calculate semantic dimension from antonym pair#
+dimension<-function(x,y) nrm(nrm(x)-nrm(y))
 
-with open('vocab_list_ngf_'+str(year_1)+'_'+str(year_2)+'_full.txt','wb') as outfile:
-    for i in range(0,len(vocab_list)):
-        outfile.write(vocab_list[i].encode('utf8')+"\n".encode('ascii'))
+###STORE EMBEDDING AS MATRIX, NORMALIZE WORD VECTORS###
+cdfm<-as.matrix(data.frame(df))
+cdfmn<-t(apply(cdfm,1,nrm))
+
+
+###IMPORT LISTS OF TERMS TO PROJECT AND ANTONYM PAIRS#####
+ant_pairs_aff <- read.csv("DIR/affluence_pairs.csv",header=FALSE, stringsAsFactor=F)
+ant_pairs_gen <- read.csv("DIR/gender_pairs.csv",header=FALSE, stringsAsFactor=F)
+ant_pairs_race <- read.csv("DIR/race_pairs.csv",header=FALSE, stringsAsFactor=F)
+
+
+word_dims<-matrix(NA,nrow(ant_pairs_aff),300)
+
+
+###SETUP "make_dim" FUNCTION, INPUT EMBEDDING AND ANTONYM PAIR LIST#######
+###OUTPUT AVERAGE SEMANTIC DIMENSION###
+
+make_dim<-function(embedding,pairs){
+word_dims<-data.frame(matrix(NA,nrow(pairs),300))
+for (j in 1:nrow(pairs)){
+rp_word1<-pairs[j,1]
+rp_word2<-pairs[j,2]
+tryCatch(word_dims[j,]<-dimension(embedding[rp_word1,],embedding[rp_word2,]),error=function(e){})
+}
+dim_ave<-colMeans(word_dims, na.rm = TRUE)
+dim_ave_n<-nrm(dim_ave)
+return(dim_ave_n)
+}
+
+
+#####CONSTRUCT AFFLUENCE, GENDER, AND RACE DIMENSIONS######
+aff_dim<-make_dim(df,ant_pairs_aff)
+gender_dim<-make_dim(df,ant_pairs_gen)
+race_dim<-make_dim(df,ant_pairs_race)
+
+
+####ANGLES BETWEEN DIMENSIONS#######
+cos(aff_dim,gender_dim)
+cos(aff_dim,race_dim)
+cos(gender_dim,race_dim)
+
+
+####CALCULATE PROJECTIONS BY MATRIX MULTIPLICATION####
+###(Equivalent to cosine similarity because vectors are normalized)###
+aff_proj<-cdfmn%*%aff_dim
+gender_proj<-cdfmn%*%gender_dim
+race_proj<-cdfmn%*%race_dim
+
+projections_df<-cbind(aff_proj, gender_proj, race_proj)
+colnames(projections_df)<-c("aff_proj","gender_proj","race_proj")
+
+
+####MERGE WITH SURVEY AND CALCULATE CORRELATION####
+projections_sub<-subset(projections_df, rownames(projections_df) %in% rownames(survey))
+colnames(projections_sub)<-c("aff_proj","gender_proj","race_proj")
+survey_proj<-merge(survey,projections_sub,by=0)
+
+
+cor(survey_proj$survey_class,survey_proj$aff_proj)
+cor(survey_proj$survey_gender,survey_proj$gender_proj)
+cor(survey_proj$survey_race,survey_proj$race_proj)
+
+########################################################################
+
+
+###CREATE VISUALIZATION###
+wlist=c("camping","baseball","boxing","volleyball","softball","golf","tennis","soccer","basketball","hockey")
+Visualization<-ggplot(data=data.frame(projections_df[wlist,]),aes(x=gender_proj,y=aff_proj,label=wlist)) + geom_text()
+Visualization+ theme_bw() +ylim(-.25,.25) +xlim(-.25,.25)
+
+########################################################################
